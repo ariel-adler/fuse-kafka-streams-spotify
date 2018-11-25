fd6cc456be633676eb596f49b29e2c8d583cb323
Yaniv Peleg <yanivpl5@gmail.com>
2018-11-25 15:57:05 +0200

    Add producers

--- stream.spotify/src/main/java/tikal.spotify/UserWeatherStream.java
+++ stream.spotify/src/main/java/tikal.spotify/UserWeatherStream.java
@@ -2,7 +2,10 @@ package tikal.spotify;
 
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.*;
 import tikal.spotify.domain.LocationWeather;
 import tikal.spotify.domain.UserLocation;
@@ -14,14 +17,21 @@ import tikal.spotify.serdes.UserLocationSerializer;
 import tikal.spotify.serdes.UserLocationDeserializer;
 
 
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-public class UserWeatherProducer {
+public class UserWeatherStream {
     public static String KAFKA_BROKERS = "localhost:9092";
     public static String LOCATION_WEATHER_TOPIC_NAME = "location_weathers";
     public static String USER_LOCATION_TOPIC_NAME = "user_locations";
     public static String OUT_TOPIC_NAME = "user_weather";
 
+    public static void main(String[] args) {
+        UserWeatherStream userWeatherStream = new UserWeatherStream();
+        userWeatherStream.joinUserWeather();
+    }
+    
     public void joinUserWeather() {
         final StreamsBuilder builder = new StreamsBuilder();
 
@@ -47,12 +57,40 @@ public class UserWeatherProducer {
                 )
         );
 
-        join.to("output", Produced.with(
+        join.to(OUT_TOPIC_NAME, Produced.with(
                 Serdes.String(),
                 UserWeatherSerdes.getSerdes()
         ));
 
-//        join.map((key, value)-> )
+        final Topology topology = builder.build();
+        final KafkaStreams streams = new KafkaStreams(topology, getProps());
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        }
+        catch (Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);
     }
 
+    private Properties getProps() {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "user-weather");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, SongsProducer.KAFKA_BROKERS);
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        return props;
+    }
 }
